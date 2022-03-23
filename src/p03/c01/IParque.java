7a7d2f99994275f50521389cc684dfd95bc9c6aa
sch1003 <thecastax@gmail.com>
2022-03-23 10:09:23 +0100

    Cambios en IParque
    Issue #2
    
    Se ha añadido el método abstracto salirDelParque(String puerta) a la interfaz.

--- src/p03/c01/IParque.java
+++ src/p03/c01/IParque.java
@@ -1,9 +1,9 @@
 package src.p03.c01;
 
 public interface IParque {
-	
+
 	public abstract void entrarAlParque(String puerta);
 
-	// TODO
-
+	public abstract void salirDelParque(String puerta);
+	
 }
