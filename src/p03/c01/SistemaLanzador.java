976ae09f17fcebbb1af0a0714e4931788ebb41e1
sch1003 <thecastax@gmail.com>
2022-03-30 09:15:57 +0200

    Implementación Método SistemaLanzador
    Issue #6
    
    Añadida la creación de hilos de salida al método main.

--- src/p03/c01/SistemaLanzador.java
+++ src/p03/c01/SistemaLanzador.java
@@ -2,27 +2,27 @@ package src.p03.c01;
 
 public class SistemaLanzador {
 	public static void main(String[] args) {
-		
+
 		IParque parque = new Parque(); // TODO
 		char letra_puerta = 'A';
-		
+
 		System.out.println("¡Parque abierto!");
-		
+
 		for (int i = 0; i < Integer.parseInt(args[0]); i++) {
-			
-			String puerta = ""+((char) (letra_puerta++));
-			
+
+			String puerta = "" + ((char) (letra_puerta++));
+
 			// Creación de hilos de entrada
 			ActividadEntradaPuerta entradas = new ActividadEntradaPuerta(puerta, parque);
-			new Thread (entradas).start();
-			
-			// 
+			new Thread(entradas).start();
+
+			//
 			// TODO
 			//
 			
-			
-			
-			
+			// Creación de hilos de salida
+			ActividadSalidaPuerta salidas = new ActividadSalidaPuerta(puerta, parque);
+			new Thread(salidas).start();
 		}
-	}	
+	}
 }
