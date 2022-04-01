eb2e64706f538a87bc7feac5f2bd9d3a8ab385f9
sch1003 <thecastax@gmail.com>
2022-04-01 12:54:44 +0200

    Documentación e identación clase ActividadEntradaPuerta
    Issue #1

--- src/p03/c01/ActividadEntradaPuerta.java
+++ src/p03/c01/ActividadEntradaPuerta.java
@@ -1,33 +1,49 @@
+// PAQUETE
 package src.p03.c01;
 
+// IMPORTACIONES DE CLASES
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class ActividadEntradaPuerta implements Runnable{
+/**
+ * Clase que implementa la clase Runnable. Implementa un hilo que representa la
+ * entrada por una puerta de un parque pasados por parámetro en el constructor
+ * de la clase.
+ * 
+ * @author Alberto Alegre Madrid
+ * @author Sergio Castañeira Hoyos
+ */
+public class ActividadEntradaPuerta implements Runnable {
 
-		private static final int NUMENTRADAS = 20;
-		private String puerta;
-		private IParque parque;
+	private static final int NUMENTRADAS = 20; // Número máximo de entradas por una puerta.
+	private String puerta; // Puerta por la que se realizan entradas.
+	private IParque parque; // Parque.
 
-		public ActividadEntradaPuerta(String puerta, IParque parque) {
-			this.puerta = puerta;
-			this.parque = parque;
-		}
+	/**
+	 * Constructor que inicializa los atributos de la clase.
+	 * 
+	 * @param puerta Puerta por la que se realizan entradas
+	 * @param parque Parque
+	 */
+	public ActividadEntradaPuerta(String puerta, IParque parque) {
+		this.puerta = puerta;
+		this.parque = parque;
+	}
 
-		@Override
-		public void run() {
-			for (int i = 0; i < NUMENTRADAS; i ++) {
-				try {
-					parque.entrarAlParque(puerta);
-					TimeUnit.MILLISECONDS.sleep(new Random().nextInt(5)*1000);
-				} catch (InterruptedException e) {
-					Logger.getGlobal().log(Level.INFO, "Entrada interrumpida");
-					Logger.getGlobal().log(Level.INFO, e.toString());
-					return;
-				}
+	@Override
+	public void run() {
+		for (int i = 0; i < NUMENTRADAS; i++) {
+			try {
+				parque.entrarAlParque(puerta);
+				TimeUnit.MILLISECONDS.sleep(new Random().nextInt(5) * 1000);
+			} catch (InterruptedException e) {
+				Logger.getGlobal().log(Level.INFO, "Entrada interrumpida");
+				Logger.getGlobal().log(Level.INFO, e.toString());
+				return;
 			}
 		}
+	}
 
 }
