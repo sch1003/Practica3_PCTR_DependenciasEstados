3f3f549add9626443c6b6662733c6ebf872e32d2
sch1003 <thecastax@gmail.com>
2022-04-01 12:55:01 +0200

    Documentación e identación clase SistemaLanzador
    Issue #1

--- src/p03/c01/SistemaLanzador.java
+++ src/p03/c01/SistemaLanzador.java
@@ -1,9 +1,21 @@
+// PAQUETE
 package src.p03.c01;
 
+/**
+ * Clase que inicia la ejecución del programa. Crea un parque y dos hilos de
+ * entrada y salida por cada puerta pasada por argumento por consola.
+ * 
+ * @author Alberto Alegre Madrid
+ * @author Sergio Castañeira Hoyos
+ */
 public class SistemaLanzador {
+	/**
+	 * Método principal de la clase que se encarga de la ejecución.
+	 * 
+	 * @param args Argumentos escritos por consola
+	 */
 	public static void main(String[] args) {
-
-		IParque parque = new Parque(); // TODO
+		IParque parque = new Parque();
 		char letra_puerta = 'A';
 
 		System.out.println("¡Parque abierto!");
@@ -16,10 +28,6 @@ public class SistemaLanzador {
 			ActividadEntradaPuerta entradas = new ActividadEntradaPuerta(puerta, parque);
 			new Thread(entradas).start();
 
-			//
-			// TODO
-			//
-			
 			// Creación de hilos de salida
 			ActividadSalidaPuerta salidas = new ActividadSalidaPuerta(puerta, parque);
 			new Thread(salidas).start();
