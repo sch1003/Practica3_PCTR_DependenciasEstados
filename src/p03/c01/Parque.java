1af3fc8bd3f4bc5519f6907d77a89a1b0cf7d992
sch1003 <thecastax@gmail.com>
2022-04-01 12:14:58 +0200

    Cambios clase Parque
    Issue #5
    Los métodos salirDelParque() y entrarAlParque() son métodos synchronized.
    Estos métodos requieren de sus comprobaciones respectivas antes de que se proceda a su ejecución y la notificación al resto de hilos de que son liberados, además de realizar un checkInvariante().

--- src/p03/c01/Parque.java
+++ src/p03/c01/Parque.java
@@ -3,113 +3,105 @@ package src.p03.c01;
 import java.util.Enumeration;
 import java.util.Hashtable;
 
-public class Parque implements IParque{
-
-
+public class Parque implements IParque {
 
 	private int contadorPersonasTotales;
 	private Hashtable<String, Integer> contadoresPersonasPuerta;
-	
-	
-	public Parque() {	
+
+	public Parque() {
 		contadorPersonasTotales = 0;
 		contadoresPersonasPuerta = new Hashtable<String, Integer>();
-		
-	}
 
+	}
 
 	@Override
-	public void entrarAlParque(String puerta){		
-		
+	public synchronized void entrarAlParque(String puerta) {
+
 		// Si no hay entradas por esa puerta, inicializamos
-		if (contadoresPersonasPuerta.get(puerta) == null){
+		if (contadoresPersonasPuerta.get(puerta) == null) {
 			contadoresPersonasPuerta.put(puerta, 0);
 		}
-		
-	
-				
+
+		comprobarAntesDeEntrar();
 		
 		// Aumentamos el contador total y el individual
-		contadorPersonasTotales++;		
-		contadoresPersonasPuerta.put(puerta, contadoresPersonasPuerta.get(puerta)+1);
-		
+		contadorPersonasTotales++;
+		contadoresPersonasPuerta.put(puerta, contadoresPersonasPuerta.get(puerta) + 1);
+
 		// Imprimimos el estado del parque
 		imprimirInfo(puerta, "Entrada");
-		
-	
-		
+		notifyAll();
+		checkInvariante();
 	}
-	
-	// 
-	//  Método salirDelParque
-public void salirDelParque(String puerta){	
+
+	// Método salirDelParque
+	@Override
+	public synchronized void salirDelParque(String puerta) {
+
+		comprobarAntesDeSalir();
 		
-				
 		// Decrementamos el contador total y el individual
-		contadorPersonasTotales--;		
-		contadoresPersonasPuerta.put(puerta, contadoresPersonasPuerta.get(puerta)-1);
-		
+		contadorPersonasTotales--;
+		contadoresPersonasPuerta.put(puerta, contadoresPersonasPuerta.get(puerta) - 1);
+
 		// Imprimimos el estado del parque
 		imprimirInfo(puerta, "Salida");
-		
-
-		
+		notifyAll();
+		checkInvariante();
 	}
-	
-	
-	private void imprimirInfo (String puerta, String movimiento){
+
+	private void imprimirInfo(String puerta, String movimiento) {
 		System.out.println(movimiento + " por puerta " + puerta);
-		System.out.println("--> Personas en el parque " + contadorPersonasTotales); //+ " tiempo medio de estancia: "  + tmedio);
-		
+		System.out.println("--> Personas en el parque " + contadorPersonasTotales); // + " tiempo medio de estancia: " +
+																					// tmedio);
+
 		// Iteramos por todas las puertas e imprimimos sus entradas
-		for(String p: contadoresPersonasPuerta.keySet()){
+		for (String p : contadoresPersonasPuerta.keySet()) {
 			System.out.println("----> Por puerta " + p + " " + contadoresPersonasPuerta.get(p));
 		}
 		System.out.println(" ");
 	}
-	
+
 	private int sumarContadoresPuerta() {
 		int sumaContadoresPuerta = 0;
-			Enumeration<Integer> iterPuertas = contadoresPersonasPuerta.elements();
-			while (iterPuertas.hasMoreElements()) {
-				sumaContadoresPuerta += iterPuertas.nextElement();
-			}
+		Enumeration<Integer> iterPuertas = contadoresPersonasPuerta.elements();
+		while (iterPuertas.hasMoreElements()) {
+			sumaContadoresPuerta += iterPuertas.nextElement();
+		}
 		return sumaContadoresPuerta;
 	}
-	
+
 	protected void checkInvariante() {
-		assert sumarContadoresPuerta() == contadorPersonasTotales : "INV: La suma de contadores de las puertas debe ser igual al valor del contador del parte";
-	
+		assert sumarContadoresPuerta() == contadorPersonasTotales
+				: "INV: La suma de contadores de las puertas debe ser igual al valor del contador del parte";
+
 	}
 
-	protected void comprobarAntesDeEntrar(){
-		
-		assert contadorPersonasTotales < 50 :"El parque esta completo. espera a que haya sitio";
-		
-		notifyAll();
-		try {
-			while(contadorPersonasTotales < 50) {
-				wait();
-			}
-		}
-			catch (InterruptedException exc){
-	            System.out.println("Parque Completo.");
-	        }
-		}
-	
+	protected void comprobarAntesDeEntrar() {
 
-	protected void comprobarAntesDeSalir(){	
-	
-		assert contadorPersonasTotales < 0 :"El parque esta vacio.No puede salir nadie";
-		notifyAll();
+		// assert contadorPersonasTotales < 50 :"El parque esta completo. espera a que
+		// haya sitio";
+
 		try {
-			while(contadorPersonasTotales < 0) {
+			if (contadorPersonasTotales == 50) {
 				wait();
 			}
-		}
-			catch (InterruptedException exc){
-	            System.out.println("Parque Vacio.");
-	        }
+		} catch (InterruptedException exc) {
+			System.out.println("Parque Completo.");
 		}
 	}
 
+	protected void comprobarAntesDeSalir() {
+
+		// assert contadorPersonasTotales < 0 :"El parque esta vacio.No puede salir
+		// nadie";
+
+		try {
+			while (contadorPersonasTotales <= 0) {
+				wait();
+			}
+		} catch (InterruptedException exc) {
+			System.out.println("Parque Vacio.");
+		}
+	}
+}
